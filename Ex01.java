3588d636888d80fd777470f6362fd45b8c6f0fc3
DaniloDBezerra <danilo1301@gmail.com>
2022-04-16 23:35:22 -0300

    testando

--- Ex01.java
+++ Ex01.java
@@ -5,7 +5,7 @@ public class Ex01 {
     public static void main(String[] args) {
 
         System.out.println("Danilo Duarte Bezerra");
-        System.out.println("Kelly + Heloísa");
+        System.out.println("Kelly e Heloísa");
 
     }
 }
