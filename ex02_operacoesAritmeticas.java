e7a4bee3583435c43d561984e049c88b599cc7e3
DaniloDuarte <91488136+DaniloDBezerra@users.noreply.github.com>
2022-05-12 17:34:17 -0300

    Update ex02_operacoesAritmeticas.java

--- ex02_operacoesAritmeticas.java
+++ ex02_operacoesAritmeticas.java
@@ -3,8 +3,7 @@ package Moderna.Modulo01.aula1;
 public class ex02_operacoesAritmeticas {
     
     /*Ex02.
-    Escreva as 4 operações aritméticas dos números 45 e 5. Crie as operações dentro de variáveis.
-    */
+    Escreva as 4 operações aritméticas dos números 45 e 5. Crie as operações dentro de variáveis.*/
 
     public static void main(String[] args){
 
@@ -15,5 +14,7 @@ public class ex02_operacoesAritmeticas {
         System.out.println("45-5= "+(45-5));
         System.out.println("45x5= "+(45*5));
         System.out.println("45:5= "+(45/5));
+        
     }
+    
 }
