26f6739a121e2674dbc9b5787d40ae946eff9acd
DaniloDuarte <91488136+DaniloDBezerra@users.noreply.github.com>
2022-05-12 17:32:32 -0300

    Update and rename exercicio3.java to ex3_mod.java

---
+++ ex3_mod.java
@@ -0,0 +1,17 @@
+package Moderna.Modulo01.aula1;
+
+public class exercicio3 {
+    
+    /*Ex03.
+    Crie duas variáveis do tipo long e com os valores 5 e 2 e imprima no console o MOD deles.*/
+
+    public static void main(String[] args) {
+
+        long num1 = 5;
+        long num2 = 2;
+
+        System.out.println("5 % 2 = " + (5 % 2) );
+        
+    }
+    
+}
