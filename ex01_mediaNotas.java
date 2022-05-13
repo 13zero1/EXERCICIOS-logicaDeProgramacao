99484ad00428540df95a8a79783802184c29b04a
DaniloDuarte <91488136+DaniloDBezerra@users.noreply.github.com>
2022-05-13 14:31:43 -0300

    Update ex01_mediaNotas.java
    EXERCÍCIO DE FIXAÇÃO

--- ex01_mediaNotas.java
+++ ex01_mediaNotas.java
@@ -4,22 +4,22 @@ public class ex01_mediaNotas {
         
         /*Ex01.
         Crie 4 variáveis do tipo double com valores (8.8 - 3.5 - 7.3 - 9.0). Calcule a média das notas e imprima no console o resultado.*/
-        
+
         //o programa começa aqui...
         public static void main(String[] args){
-            
-            //declarando as variaveis...
-            double nota1 = (8.8);
-            double nota2 = (7.5);
-            double nota3 = (1.3);
-            double nota4 = (9.0);
-            
-            //calculando...
-            double media = ((nota1+nota2+nota3+nota4)/4);
-              
-            //imprimindo...    
-            System.out.println("A média é " + media);
-                
+
+        //declarando as variaveis...
+        double nota1 = (8.8);
+        double nota2 = (7.5);
+        double nota3 = (1.3);
+        double nota4 = (9.0);
+
+        //calculando...
+        double media = ((nota1+nota2+nota3+nota4)/4);
+
+        //imprimindo...    
+        System.out.println("A média é " + media);
+
         }
         
 }
