76ae4e24e5c41c37649c6b679eacb4712a5761ce
DaniloDuarte <91488136+DaniloDBezerra@users.noreply.github.com>
2022-05-13 14:29:56 -0300

    Update and rename exercicio10.java to ex10_idadeVoto.java

---
+++ ex10_idadeVoto.java
@@ -0,0 +1,30 @@
+package Moderna.Modulo01.aula1;
+
+import java.util.Scanner;
+
+public class ex10_idadeVoto {
+    
+    /*Ex10.
+    Escreva um programa utilizando operador ternário que imprima se o voto é proibido ou permitido, utilizando como regra:
+        ● Menor ou igual a 18 anos = voto proibido.
+        ● Maior que 18 anos = voto obrigatório*/
+
+    public static void main (String[] args) {
+
+        Scanner scan = new Scanner (System.in);
+
+        System.out.println ("Qual é a sua idade?");
+        System.out.print ("R:");
+        int idadeUsuario = scan.nextInt();
+
+        if (idadeUsuario <= 18) {
+            System.out.println ("Voto Proibido");
+        
+        } else {
+            System.out.println ("Voto obrigatório");
+    
+        }
+
+    }
+
+}
