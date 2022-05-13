d5d8c47816f377baee38b5c083ac9bf69eb81fb7
DaniloDuarte <91488136+DaniloDBezerra@users.noreply.github.com>
2022-05-13 14:34:22 -0300

    Update and rename Ex01.java to Ex01_nomeCompleto.java

---
+++ Ex01_nomeCompleto.java
@@ -0,0 +1,13 @@
+public class Ex01 {
+
+    /*Ex01_
+    Escreva um programa que imprima no console o seu nome completo e de duas pessoas que você gosta.*/
+
+    public static void main(String[] args) {
+
+        System.out.println ("Danilo Duarte Bezerra");
+        System.out.println ("Kelly e Heloísa");
+
+    }
+
+}
