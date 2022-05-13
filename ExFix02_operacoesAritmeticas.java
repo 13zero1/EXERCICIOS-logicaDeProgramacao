a746cef0d44f5dba230bcf1f1e849459ecac47d9
DaniloDuarte <91488136+DaniloDBezerra@users.noreply.github.com>
2022-05-13 14:36:26 -0300

    Update and rename ex02_operacoesAritmeticas.java to ExFix02_operacoesAritmeticas.java

--- ExFix02_operacoesAritmeticas.java
+++ ExFix02_operacoesAritmeticas.java
@@ -1,6 +1,4 @@
-package Moderna.Modulo01.aula1;
-
-public class ex02_operacoesAritmeticas {
+public class ExFix02_operacoesAritmeticas {
     
     /*Ex02.
     Escreva as 4 operações aritméticas dos números 45 e 5. Crie as operações dentro de variáveis.*/
