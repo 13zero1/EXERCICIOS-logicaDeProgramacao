300809914a5a1ba10a265f28a12b41487f71eba9
DaniloDuarte <91488136+DaniloDBezerra@users.noreply.github.com>
2022-05-13 13:57:23 -0300

    Update and rename exercicio6.java to ex06_trocandoValores.java

---
+++ ex06_trocandoValores.java
@@ -0,0 +1,24 @@
+package Moderna.Modulo01.aula1;
+
+public class ex06_trocandoValores {
+    
+    /*Ex06.
+    Desenvolva um programa que tenha duas variáveis do tipo String. 
+    Efetue a troca dos valores de uma com a outra e imprima no console com os valores trocados.*/
+
+    public static void main (String[] args) {
+
+        String a = "Colher";
+        String b = "Garfo";
+        String c;
+
+        c = b;
+        b = a;
+        a = c;
+
+        System.out.println (" o valor de a é " + a );
+        System.out.println (" o valor de b é " + b );
+
+    }
+
+}
