a14a3133a9a69a169116b2a211f8bc229c405acc
DaniloDuarte <91488136+DaniloDBezerra@users.noreply.github.com>
2022-05-13 14:52:47 -0300

    Update ex05.java

--- ex05.java
+++ ex05.java
@@ -1,15 +1,23 @@
-package Moderna.Modulo01.aula1;
-
 public class ex05 {
 
-    /*Crie um programa com duas variáveis do tipo long com os valores 15 e 10,
-    imprima no console o resultado do MOD das duas variáveis.*/
+    /*Ex05_
+    Crie um programa com duas variáveis do tipo long com os valores 15 e 10, imprima no console o resultado do MOD das duas variáveis.*/
 
     public static void main(String[] args) {
 
         long num1 = 15;
         long num2 = 10;
 
-        System.out.println("15 : 10 = " + (15%10));  // >>>> MOD É ISSO??
+        //usando o MOD...
+        System.out.println("15 : 10 = " + (15%10));
+
+//        MOD ( % ) - o resto da divisão.
+//        ex.: 
+//        2 % 2 = 0 
+//        2 : 2 = 1, com resto igual '0'
+//        3 % 2 = 1
+//        3 : 2 = 1 com resto diferente de '0'
+    
     }
+
 }
